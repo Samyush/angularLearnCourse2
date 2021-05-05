5da752d0dc90ee36dfc09f320946efcf3b388365
samyush <samyushmaharjan@gmail.com>
2021-05-05 22:39:13 +0545

    use of injectionToken(inside this one) is unknown

--- src/app/providers.ts
+++ src/app/providers.ts
@@ -1,5 +1,6 @@
 import {InjectionToken} from '@angular/core';
 
+// the desc in the injection token (string in side this) function is unknown, works fine even on change
 export const  lookupListToken = new InjectionToken('lookupListToken');
 
 export const lookupLists = {mediums: ['Movies', 'Series']};
